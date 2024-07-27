38f915d87bc52dfbc14b92a5bd12189cbb81c711
Himanshu <hgusain088@gmail.com>
2024-07-27 07:48:25 +0530

    make header and navbar

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["img.daisyui.com", "web.biorex.mycloudcinema.com"]
+  }
+}
 
-export default nextConfig;
+export default nextConfig
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google"
 import "./globals.css"
+import Header from "@/components/Header"
 import Navbar from "@/components/Navbar"
 
 const inter = Inter({ subsets: ["latin"] })
@@ -12,8 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Navbar />
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        <Navbar />
+        {children}
+      </body>
     </html>
   )
 }
